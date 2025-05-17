16769198ea7f970f446e5cd687db11a3e7e413a2
rgr14 <rogeraugustosoares@gmail.com>
2025-05-17 14:42:37 -0300

    add - documentação

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Query
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from jose import jwt, JWTError
 from security import get_current_user
@@ -10,9 +10,14 @@ from database import SessionLocal
 from models.scraped_data import ScrapedData
 from utils.cache import salvar_scraping
 import json
-from fastapi import Query
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
-app = FastAPI()
+app = FastAPI(
+    title="API Vitivinicultura", 
+    description="API pública baseada em dados da Embrapa.",
+    version="1.0"
+)
 
 # Autenticação
 security_basic = HTTPBasic()
@@ -23,13 +28,38 @@ def login(credentials: HTTPBasicCredentials = Depends(security_basic)):
     if credentials.username not in users or users[credentials.username] != credentials.password:
         raise HTTPException(status_code=401, detail="Credenciais inválidas")
 
-    token = jwt.encode({"sub": credentials.username}, SECRET_KEY, algorithm=ALGORITHM)
-    return {"access_token": token, "token_type": "bearer"}
+    sao_paulo_tz = ZoneInfo("America/Sao_Paulo")
+    now = datetime.now(sao_paulo_tz)
+    expiration = now + timedelta(hours=1)
 
-@app.get("/dados-producao")
+    payload = {
+        "sub": credentials.username,
+        "iat": int(now.timestamp()),
+        "exp": int(expiration.timestamp())
+    }
+
+    token = jwt.encode({"sub": credentials.username}, SECRET_KEY, algorithm=ALGORITHM)
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+        'expired_at': expiration.isoformat()
+    }
+
+
+@app.get(
+    "/dados-producao",
+    summary="Consulta dados de produção",
+    description="Retorna dados de produção de produtos da vitivinicultura com base na Embrapa. "
+                "É possível filtrar os dados por ano (1970 a 2023). Se o site estiver offline, dados são retornados do cache local."
+)
 def get_data_production(
     username: str = Depends(get_current_user),
-    ano: int = Query(None, ge=1970, le=2023)
+    ano: int = Query(
+        default=1970,
+        description="Ano da produção vitivinícola (entre 1970 e 2023)",
+        ge=1970,
+        le=2023
+    )
 ):
     try:
         base_url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02'
@@ -70,11 +100,21 @@ def get_data_production(
         else:
             raise HTTPException(status_code=503, detail="Fonte indisponível e sem cache local.")
 
-    
+@app.get(
+    "/dados-processamento",
+    summary="Consulta dados de processamento",
+    description="Retorna dados de processamento de produtos da vitivinicultura com base na Embrapa. "
+                "É possível filtrar os dados por ano (1970 a 2023). Se o site estiver offline, dados são retornados do cache local."
+)
 @app.get("/dados-processamento")
 def get_data_processing(
     username: str = Depends(get_current_user),
-    ano: int = Query(None, ge=1970, le=2023)
+        ano: int = Query(
+        default=1970,
+        description="Ano da produção vitivinícola (entre 1970 e 2023)",
+        ge=1970,
+        le=2023
+    )
 ):
     urls = [
         'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_01&opcao=opt_03',
@@ -125,11 +165,20 @@ def get_data_processing(
         raise HTTPException(status_code=503, detail="Nenhuma fonte disponível ou cache encontrado.")
     return resultados
 
-    
-@app.get("/dados-comercializacao")
+@app.get(
+    "/dados-comercializacao",
+    summary="Consulta dados de comercializacao",
+    description="Retorna dados de comercializacao de produtos da vitivinicultura com base na Embrapa. "
+                "É possível filtrar os dados por ano (1970 a 2023). Se o site estiver offline, dados são retornados do cache local."
+)
 def get_data_commercialization(
     username: str = Depends(get_current_user),
-    ano: int = Query(None, ge=1970, le=2023)
+        ano: int = Query(
+        default=1970,
+        description="Ano da produção vitivinícola (entre 1970 e 2023)",
+        ge=1970,
+        le=2023
+    )
 ):
     base_url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_04'
 
@@ -171,11 +220,21 @@ def get_data_commercialization(
                 "dados": json.loads(cache.dados_json)
             }
         raise HTTPException(status_code=503, detail="Fonte indisponível e sem cache local.")
-    
-@app.get("/dados-importacao")
+
+@app.get(
+    "/dados-importacao",
+    summary="Consulta dados de importação",
+    description="Retorna dados de importação de produtos da vitivinicultura com base na Embrapa. "
+                "É possível filtrar os dados por ano (1970 a 2024). Se o site estiver offline, dados são retornados do cache local."
+)
 def get_data_import(
     username: str = Depends(get_current_user),
-    ano: int = Query(None, ge=1970, le=2024)
+        ano: int = Query(
+        default=1970,
+        description="Ano da produção vitivinícola (entre 1970 e 2023)",
+        ge=1970,
+        le=2024
+    )
 ):
     urls = [
         'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_01&opcao=opt_05',
@@ -228,10 +287,21 @@ def get_data_import(
         raise HTTPException(status_code=503, detail="Nenhuma fonte disponível ou cache encontrado.")
     return resultados
     
+@app.get(
+    "/dados-exportacao",
+    summary="Consulta dados de importação",
+    description="Retorna dados de exportação de produtos da vitivinicultura com base na Embrapa. "
+                "É possível filtrar os dados por ano (1970 a 2024). Se o site estiver offline, dados são retornados do cache local."
+)
 @app.get("/dados-exportacao")
 def get_data_export(
     username: str = Depends(get_current_user),
-    ano: int = Query(None, ge=1970, le=2024)
+        ano: int = Query(
+        default=1970,
+        description="Ano da produção vitivinícola (entre 1970 e 2023)",
+        ge=1970,
+        le=2024
+    )
 ):
     urls = [
         'http://vitibrasil.cnpuv.embrapa.br/index.php?subopcao=subopt_01&opcao=opt_06',
